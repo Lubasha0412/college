ee0c114a81e516703f04ada14c7494cf008ba9b6
Любаша <144107065+Lubasha0412@users.noreply.github.com>
2023-09-23 10:55:27 +0500

    Update index.php

--- pr5/index.php
+++ pr5/index.php
@@ -5,32 +5,45 @@
     $password   = "gS7iI3fP5c";               // Пароль
     $connection = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $password);
 
+    try {
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "База данных успешно подключена";
+    }
+    catch (PDOException $e) {
+        echo "Ошибка подключения: " . $e->getMessage();
+    }
+
     $result = $connection->query('SELECT * FROM `product`');
 
     if ($result) {
-    if ($result->rowCount() == 0) {
-        echo "Таблица пустая";
-      } else {
-    echo '<table cellspacing="1" bgcolor="#000000">';
-    echo '<tr bgcolor="#gren">';
-    for ($i=0; $i < $result ->columnCount (); $i++)
-        {$column=$result ->getColumnMeta($i);
-    echo "<th>{$column['name']}</th>";
-    }
-    echo '</tr>';
 
-    while($row = $result->fetch( PDO::FETCH_ASSOC )){
-        echo '<tr bgcolor="#ffffff">';
-        foreach ($row as $key => $value) {
-            echo '<td >'.$value.'</td>';
+        if ($result->rowCount() == 0) {
+            echo "Таблица пустая";
+        } 
+        
+        else {
+        echo '<table cellspacing="1" bgcolor="#000000">';
+            echo '<tr bgcolor="#gren">';
+            for ($i=0; $i < $result ->columnCount (); $i++)
+                {$column=$result ->getColumnMeta($i);
+                echo "<th>{$column['name']}</th>";
+                }
+            echo '</tr>';
+
+        while($row = $result->fetch( PDO::FETCH_ASSOC )){
+            echo '<tr bgcolor="#ffffff">';
+                foreach ($row as $key => $value) {
+                echo '<td >'.$value.'</td>';
+            }
+        echo '</tr>';
+        } 
+        echo '</table>';} 
         }
-       echo '</tr>';
-    } 
-    echo '</table>';
-    } }
-      else {
+
+    else {
         echo 'Запрос выполнен с ошибкой';
     }
 ?>   
+    
 
-
+?>
