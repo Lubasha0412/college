ba451655548045003f035a8bc7b747ab8c06ac1c
Любаша <144107065+Lubasha0412@users.noreply.github.com>
2023-09-30 11:31:02 +0500

    Create action.php

---
+++ pr7/action.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pr7</title>
+</head>
+<body>
+    <?php 
+        if( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+
+            if(empty($_POST['login'])) exit('Не все поля формы заполнены');
+            if(empty($_POST['mail'])) exit('Не все поля формы заполнены');
+            if(empty($_POST['password'])) exit('Не все поля формы заполнены');
+            if(empty($_POST['password1'])) exit('Не все поля формы заполнены');
+
+            if($_POST['password'] !== $_POST['password1']) exit('Пароли не совпадают');
+
+            
+
+            echo 'Здравствуйте, '.htmlspecialchars($_POST['login']).'. <br>';
+            echo 'Ваш акаун создан';
+        }
+    ?>
+</body>
+</html>
