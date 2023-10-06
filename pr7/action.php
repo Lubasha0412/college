f6756f6c6c84d7a0ee756b991287c116f0313cd1
Любаша <144107065+Lubasha0412@users.noreply.github.com>
2023-10-06 11:46:11 +0500

    Update action.php

--- pr7/action.php
+++ pr7/action.php
@@ -8,6 +8,12 @@
 </head>
 <body>
     <?php 
+    $host       = "db4.myarena.ru";      // Адрес сервера базы данных
+    $dbname     = "u19978_b08";    // Имя базы данных
+    $user       = "u19978_b08";           // Имя пользователя
+    $password   = "gS7iI3fP5c";               // Пароль
+    $connection = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $password);
+
         if( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 
             if(empty($_POST['login'])) exit('Не все поля формы заполнены');
@@ -17,13 +23,24 @@
 
             if($_POST['password'] !== $_POST['password1']) exit('Пароли не совпадают');
 
-            include("index.php");
             $len = strlen($_POST["password"]);
             if($len < 8) exit ('Минимальное количесво значенией 8. Введите пароль занова');
-            else{
-            echo 'Здравствуйте, '.htmlspecialchars($_POST['login']).'. <br>';
-            echo 'Ваш акаун создан';}
         }
+        
+        $select  = $connection->prepare( "SELECT COUNT(`id`) as cnt FROM `user` where `login`=?;");
+        $res = $select->execute([$_POST['login']]);
+        $rew = $select->fetch();
+        if(!$res){
+            exit('Ошибка регистрации (Такой пользователь уже есть)');
+        }
+        //Добавление нового пользователя
+        $data = [$_POST['login'],$_POST['mail'],$_POST['password']];
+        $res = $connection->prepare("INSERT INTO `user` (`login`,`mail`,`pasword`) VALUES (?,?,?);");
+        $res = $res->execute($data);
+
+        if ($res) {exit ('Регистрация прошла успешно');
+        }
+        exit ('Ошибка регисьтрации');
     ?>
 </body>
 </html>
