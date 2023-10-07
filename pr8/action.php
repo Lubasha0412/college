4b6944031838fec24e93b7777b1ec1972ba5044d
Любаша <144107065+Lubasha0412@users.noreply.github.com>
2023-10-07 10:42:49 +0500

    Create action.php

---
+++ pr8/action.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>pr8</title>
+</head>
+<body>
+    <?php 
+        $host       = "db4.myarena.ru";      // Адрес сервера базы данных
+        $dbname     = "u19978_b08";    // Имя базы данных
+        $user       = "u19978_b08";           // Имя пользователя
+        $password   = "gS7iI3fP5c";               // Пароль
+        $connection = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $password);
+
+        $link = '<a href = "./index.php">На главную</a>';
+
+        if( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+
+            if(empty($_POST['login'])) exit('Не все поля формы заполнены'.'<br>'.$link);
+            if(empty($_POST['password'])) exit('Не все поля формы заполнены'.'<br>'.$link);
+        }
+        
+        $stmt = $connection->prepare("SELECT * FROM `user` WHERE login = :login");
+        $stmt->execute(['login' => $_POST['login']]);
+        if (!$stmt->rowCount()) {
+            exit('Пользователь с такими данными не зарегистрирован'.'<br>'.$link);
+            die;
+        }
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // проверяем пароль
+        if (password_verify($_POST['password'], $user['password'])) {
+            if (password_needs_rehash($user['password'], PASSWORD_DEFAULT)) {
+                $newHash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+                $stmt = $connection->prepare('UPDATE `user` SET `password` = :password WHERE `login` = :login');
+                 $stmt->execute(['login' => $_POST['login'],'password' => $newHash,]);
+            }     
+        
+            exit('Пароль неверен'.'<br>'.$link);
+        }
+        else {
+            exit('Вход выполнен успешно'.'<br>'.$link); 
+        }
+    ?>
+</body>
+</html>
