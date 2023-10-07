fab5403780bd97a53da9a2b9e4f0f8e48f50078e
Любаша <144107065+Lubasha0412@users.noreply.github.com>
2023-10-07 09:24:51 +0500

    Update action.php

--- pr7/action.php
+++ pr7/action.php
@@ -14,33 +14,43 @@
     $password   = "gS7iI3fP5c";               // Пароль
     $connection = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $password);
 
+        $link = '<a href = "./index.php">На главную</a>';
+
         if( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 
-            if(empty($_POST['login'])) exit('Не все поля формы заполнены');
-            if(empty($_POST['mail'])) exit('Не все поля формы заполнены');
-            if(empty($_POST['password'])) exit('Не все поля формы заполнены');
-            if(empty($_POST['password1'])) exit('Не все поля формы заполнены');
+            if(empty($_POST['login'])) exit('Не все поля формы заполнены'.'<br>'.$link);
+            if(empty($_POST['mail'])) exit('Не все поля формы заполнены'.'<br>'.$link);
+            if(empty($_POST['password'])) exit('Не все поля формы заполнены'.'<br>'.$link);
+            if(empty($_POST['password1'])) exit('Не все поля формы заполнены'.'<br>'.$link);
 
-            if($_POST['password'] !== $_POST['password1']) exit('Пароли не совпадают');
+            if($_POST['password'] !== $_POST['password1']) exit('Пароли не совпадают'.'<br>'.$link);
 
             $len = strlen($_POST["password"]);
-            if($len < 8) exit ('Минимальное количесво значенией 8. Введите пароль занова');
+            if($len < 8) exit ('Минимальное количесво значенией 8. Введите пароль занова'.'<br>'.$link);
         }
         
-        $select  = $connection->prepare( "SELECT COUNT(`id`) as cnt FROM `user` where `login`=?;");
-        $res = $select->execute([$_POST['login']]);
-        $rew = $select->fetch();
-        if(!$res){
-            exit('Ошибка регистрации (Такой пользователь уже есть)');
+        $select = $connection->prepare( "SELECT COUNT(`id`) as cnt FROM `user` WHERE `login` = ? OR `mail` = ?;"); 
+        $res = $select->execute( [ $_POST['login'], $_POST['mail']  ] );
+        $row = $select->fetch(); 
+        if(  !$res && !isset($row['cnt']) ) { 
+            exit( "Ошибка регистрации… (3)".'<br>'.$link);
         }
+
+        if( $row[0] > 0){
+        exit( "Ошибка регистрации… (Пользователь уже существует)".'<br>'.$link);
+        }
+
+        $pas_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+        $data = [$_POST['login'],$_POST['mail'],$pas_hash];
+
         //Добавление нового пользователя
-        $data = [$_POST['login'],$_POST['mail'],$_POST['password']];
         $res = $connection->prepare("INSERT INTO `user` (`login`,`mail`,`pasword`) VALUES (?,?,?);");
         $res = $res->execute($data);
 
-        if ($res) {exit ('Регистрация прошла успешно');
+        if ($res) {exit ('Регистрация прошла успешно'.'<br>'.$link);
         }
-        exit ('Ошибка регисьтрации');
+        exit ('Ошибка регисьтрации'.'<br>'.$link);
     ?>
 </body>
 </html>
