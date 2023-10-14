5843f7a98303436faae9ee771a7e3def2008e1b5
Любаша <144107065+Lubasha0412@users.noreply.github.com>
2023-10-14 09:39:03 +0500

    Create index.php

---
+++ PHP/pr5/index.php
@@ -0,0 +1,48 @@
+<?php
+    $host       = "db4.myarena.ru";      // Адрес сервера базы данных
+    $dbname     = "u19978_b08";    // Имя базы данных
+    $user       = "u19978_b08";           // Имя пользователя
+    $password   = "gS7iI3fP5c";               // Пароль
+    $connection = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset=utf8', $user, $password);
+
+    try {
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "База данных успешно подключена";
+    }
+    catch (PDOException $e) {
+        echo "Ошибка подключения: " . $e->getMessage();
+    } 
+
+    $result = $connection->query('SELECT * FROM `product`');
+
+    if ($result) {
+
+        if ($result->rowCount() == 0) {
+            echo "Таблица пустая";
+        } 
+        
+        else {
+            echo '<table cellspacing="1" bgcolor="#000000">';
+                echo '<tr bgcolor="#gren">';
+                for ($i=0; $i < $result ->columnCount (); $i++)
+                    {$column=$result ->getColumnMeta($i);
+                    echo "<th>{$column['name']}</th>";
+                    }
+                echo '</tr>';
+
+            while($row = $result->fetch( PDO::FETCH_ASSOC )){
+                echo '<tr bgcolor="#ffffff">';
+                    foreach ($row as $key => $value) {
+                    echo '<td >'.$value.'</td>';
+                }
+            echo '</tr>';
+            } 
+            echo '</table>';
+        } 
+    }
+
+    else {
+        echo 'Запрос выполнен с ошибкой';
+    }
+?>   
+    
