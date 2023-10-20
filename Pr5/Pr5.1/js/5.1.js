acec6e33455efbab52880e1b9240abc5b5d2950a
Любаша <144107065+Lubasha0412@users.noreply.github.com>
2023-10-20 10:14:47 +0500

    5.1

--- Pr5/Pr5.1/js/5.1.js
+++ Pr5/Pr5.1/js/5.1.js
@@ -1,21 +1,19 @@
-let log = prompt('Введите логин');
+let a = prompt('Введите логин');
 
 
-if (log == 'Админ') {
-} else  if (log=='' || log==null){
+if (a == 'Админ') {
+    let b = prompt('Пароль')
+        if (b == 'Я главный') {
+            alert ("Здравствуйте!")
+        } else  if (b == '' || b ==null){
+            alert ("Отменено")
+        }
+        else {
+            alert ("Неверный пароль")
+        } 
+} else  if (a=='' || a==null){
     alert ("Отменено")
 }
 else {
     alert ("Неверный логин")
-}
-
-let password = prompt('Введите пароль');
-
-if ( password == 'Я главный') {
-    alert ("Здравствуйте!")
-} else  if (password  == '' || password == null){
-    alert ("Отменено")
-}
-else {
-    alert ("Неверный пароль")
 }
--- Pr5/Pr5.2/js/5.2.js
+++ Pr5/Pr5.2/js/5.2.js
@@ -1,6 +1,6 @@
 let time = prompt('Введите время(в часах)');
 
-if (time < 9 || time > 17) {
+if (time < 9 && time > 17) {
     alert ("Колледж закрыт!")
 } else {
     alert ("Колледж открыт!")
