f788fbade138641ea07122dc29230e2f95762506
Любаша <144107065+Lubasha0412@users.noreply.github.com>
2023-10-21 09:11:23 +0500

    Create 6.2.js

---
+++ Pr6/Pr6.2/js/6.2.js
@@ -0,0 +1,12 @@
+let a = true;
+
+    if (a === true) {
+        alert ("Верно");
+    }else {
+        alert ("Неверно");
+    }
+let b = false;
+
+    (b === true) ? alert ("Верно") : alert ("Неверно");
+
+
