a9cc8b64a8bb16997cfdd192e9b9805465b46124
Любаша <144107065+Lubasha0412@users.noreply.github.com>
2023-10-28 10:59:12 +0500

    Update 7.8.js

--- Pr7/Pr7.8/js/7.8.js
+++ Pr7/Pr7.8/js/7.8.js
@@ -1,8 +1,8 @@
 let arr = [10,20,30,50,235,3000];
 
 for (let i = 0; i < arr.length; i++){
-    let firstDigit = String (arr[i])[0];
-    if (firstDigit === '1' || firstDigit === '2' || firstDigit === '5'){
-        console.log(arr[i])
+    let firstDigit = Math.floor(arr[i] / 10);
+    if (firstDigit === 1 || firstDigit === 2 || firstDigit === 5){
+        console.log(arr[i]);
     }
 }
