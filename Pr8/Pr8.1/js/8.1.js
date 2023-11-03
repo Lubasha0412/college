b60708237df35ab28a01a9e96239ba1410023745
Любаша <144107065+Lubasha0412@users.noreply.github.com>
2023-11-03 10:38:57 +0500

    pr8

---
+++ Pr8/Pr8.1/js/8.1.js
@@ -0,0 +1,31 @@
+let surname = "Саначева ";
+
+let name = "Любовь ";
+
+let patronymic = "Николаевна ";
+
+let ocupation = "Студент ";
+
+let degree = "Среднее ";
+
+let categry = "Первая ";
+
+let education = "Пермский краевой колледж \"ОНИКС\" ";
+
+let discipline = "Информатика ";
+
+let note = "...";
+
+document.write('<h3>' + surname + name + patronymic + '</h3>' );
+
+document.write('<i>Должность: </i>' + ocupation + '<br>' + '<br>');
+
+document.write('<i>Уровень образования: </i>' + degree + '<br>' + '<br>');
+
+document.write('<i>Категория: </i>' + categry + '<br>' + '<br>');
+
+document.write('<i>Образование: </i>' + education + '<br>' + '<br>');
+
+document.write('<i>Преподаваемая дисциплина: </i>' + discipline + '<br>' + '<br>');
+
+document.write('<i>Примечание: </i>' + note);
---
+++ Pr8/Pr8.3/js/8.3.js
@@ -0,0 +1,11 @@
+let arr = {
+    surname: "Саначева ",
+    name: "Любовь ",
+    patronymic: "Николаевна",
+    ocupation: "Студент",
+    degree: "Среднее",
+    categry: "Первая",
+    education: "Пермский краевой колледж \"ОНИКС\"",
+    discipline: "Информатика",
+    note: "...",
+};
