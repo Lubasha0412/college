e46ef1e06cc85910e289c2a17ec9a25b686fa2f5
Любаша <144107065+Lubasha0412@users.noreply.github.com>
2023-11-25 10:40:09 +0500

    10

--- Pr10/Pr10.5/js/10.5.js
+++ Pr10/Pr10.5/js/10.5.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", hiddenCloseclick);
 document.getElementById('button').addEventListener("click", hiddenCloseclick);
 function hiddenCloseclick(event) {
   let x = document.getElementById('button2');
-  x.classList.toggle('is-show');
   if (x.style.display == "none") {
     x.style.display = "block";
   } else {
